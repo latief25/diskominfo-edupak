e9dca623d1b586a226b2173ce5794fbb0c1f15f6
Muhammad Sayuti <sayutizxc@gmail.com>
2021-11-19 15:23:50 +0800

    lanjutan hari ini

--- resources/views/Dashboard/Admin/list_user.blade.php
+++ resources/views/Dashboard/Admin/list_user.blade.php
@@ -22,6 +22,27 @@
     </tfoot>
   </table>
 </div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-fullscreen-sm-down">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
 
 
@@ -83,15 +104,15 @@
         });
       },
       dom: 'lrtip',
+      "lengthMenu": [
+        [10, 25, 50],
+        [10, 25, 50]
+      ],
       "bPaginate": true,
       "bLengthChange": true,
       "bFilter": true,
       "bSort": true,
       "bAutoWidth": true,
-      "lengthMenu": [
-        [10, 25, 50],
-        [10, 25, 50]
-      ],
       "columnDefs": [{
         "orderable": false,
         "targets": [0, 3]
@@ -101,7 +122,6 @@
         return: false
       },
       serverSide: true,
-
       "ajax": "{{route('daftar.pegawai')}}",
       "columns": [{
           "data": null,
@@ -120,7 +140,7 @@
           "data": null,
           "sortable": false,
           render: function() {
-            return '<div class="d-flex justify-content-evenly"><i class="btn btn-primary fas fa-eye"></i><i class="btn btn-warning fas fa-edit"></i></div>';
+            return '<div class="d-flex justify-content-evenly"><i class="btn btn-primary fas fa-eye" data-bs-toggle="modal" data-bs-target="#editModal"></i><i class="btn btn-warning fas fa-edit"></i></div>';
           }
         },
       ]
